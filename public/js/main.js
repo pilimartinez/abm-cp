e658fe8718a074408b1584bc82bdd4011712e1aa
pilimartinez <pilartcast@hotmail.com>
2019-09-09 21:52:06 -0300

    modal

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,12 @@
 fetch('/api/users')
     .then(response => response.json())
     .then(res =>console.log(res))
+
+    const showModal = () => {
+        let modal = document.getElementById("modal");
+        if (modal.style.display === "block") {
+            modal.style.display = "none";
+        } else {
+            modal.style.display = "block";
+        }
+    }
